0969338c2147b6b59539d5198f1d7a8c08c932d4
Alex Mages <alemages@gmail.com>
2019-05-22 22:10:45 -0500

    Added Semantics-UI, created Flag.js and App.js for testing

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Flag from "./Flag";
+
+class App extends React.Component {
+
+
+    render(){
+        return(
+            <div><Flag /></div>
+        );
+    }
+}
+
+export default App;
---
+++ src/components/Flag.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Flag() {
+    return <div><i class ="us flag" /></div>
+}
+
+export default Flag;
